8ece61fec2cff9f624d3a7cedcdff67ba36ab18e
Lhagvaa <lhagvabatulzii@gmail.com>
2024-01-07 20:53:23 +0800

    New game button event listner

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
-// toglogchiin eeljiig hadgalah huwisagch , 1r toglogch 0,  2r toglogchiig 1 gej temdegley
-
-var activePlayer = 0;
-
-//Tolglogchiin tsugluulsan onoog hadgalah huwisagch
-
-var scores = [0, 0];
-
-//Idewhtwei toglogchiin eeljiin onoog hadgalah huwisagch
-
-var roundScore = 0;
-
-//Shoonii ali talaaraa buusniiig hadgalah huwisagch , 1-6 hurtel utgiig sanamsargvi baidlaar awah
-
-var diceNumber = Math.floor(Math.random() * 6) + 1;
-
-// DOM <div class="player-score" id="score-0">43</div>
-
-// document.querySelector('#score-0').textContent = dice;
-// document.querySelector('#score-1').innerHTML = "<em>YES!<em>";
-
-// Program ehlehed beltgy
-
-document.getElementById("score-0").textContent = 0;
-document.getElementById("score-1").textContent = 0;
-
-document.getElementById("current-1").textContent = 0;
-document.getElementById("current-0").textContent = 0;
-
+var activePlayer;
+var scores;
+var roundScore;
 // document.querySelector('.dice').style.display = 'none';
 var diceDom = document.querySelector(".dice");
+
+// initGame();
+
+function initGame() {
+  activePlayer = 0;
+  scores = [0, 0];
+  roundScore = 0;
+
+  //Shoonii ali talaaraa buusniiig hadgalah huwisagch , 1-6 hurtel utgiig sanamsargvi baidlaar awah
+
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+  // DOM <div class="player-score" id="score-0">43</div>
+
+  // document.querySelector('#score-0').textContent = dice;
+  // document.querySelector('#score-1').innerHTML = "<em>YES!<em>";
+
+  // Program ehlehed beltgy
+
+  document.getElementById("score-0").textContent = 0;
+  document.getElementById("score-1").textContent = 0;
+
+  document.getElementById("current-1").textContent = 0;
+  document.getElementById("current-0").textContent = 0;
+  diceDom.style.display = "none";
+
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+
+  document.querySelector(".layer-1-panel").classList.remove("winner");
+  document.querySelector(".layer-0-panel").classList.remove("winner");
+
+  document.querySelector(".layer-1-panel").classList.remove("active");
+  document.querySelector(".layer-0-panel").classList.remove("active");
+
+  document.querySelector(".layer-0-panel").classList.add("active");
+}
 
 // shoo buulgaad buulgsn utgiig randomoor ogon delgetsend haruulah(event listener)
 document.querySelector(".btn-roll").addEventListener("click", function () {
@@ -86,7 +97,4 @@ function switchtonextPlayer() {
 }
 
 // shine game ehlvvleh tobchnii event listener
-
-document.querySelector(".btn-new").addEventListener("click", function () {
-  alert("New game");
-});
+document.querySelector(".btn-new").addEventListener("click", initGame);
