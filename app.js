176614decad6090369cf1934b1779b7472b35c62
Lhagvaa <lhagvabatulzii@gmail.com>
2024-01-07 15:26:40 +0800

    toglogchiin eeljiig solidog bolgono

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // toglogchiin eeljiig hadgalah huwisagch , 1r toglogch 0,  2r toglogchiig 1 gej temdegley
  
-var activePlayer = 1;
+var activePlayer = 0;
 
 //Tolglogchiin tsugluulsan onoog hadgalah huwisagch
 
@@ -31,10 +31,33 @@ document.getElementById('current-0').textContent = 0;
 // document.querySelector('.dice').style.display = 'none';
 var diceDom = document.querySelector(".dice");
 
+
+// shoo buulgaad buulgsn utgiig randomoor ogon delgetsend haruulah(event listener)
 document.querySelector('.btn-roll').addEventListener('click', function (){
     var diceNumber = Math.floor(Math.random() * 6) +1;
-
+    //shoonii zurgiig web deer haruulah
     diceDom.style.display = 'block';
+    //buusan shoonii toog haruulah
     diceDom.src= 'dice-' + diceNumber +'.png';
+//Buusan too ni 1s ylgaatai bol idewtei toglogchiin eeljiin toog nemegdvvlne
 
+if(diceNumber !==1){
+    //1-s ylgaatai vyd idewtei toglogch shoogoo haysaar baina
+    roundScore = roundScore +  diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+}else{
+    roundScore=0;
+    document.getElementById("current-" + activePlayer).textContent= 0;
+    
+    //toglogchiin eeljin deer tsugluulsan onoog 0 bolgono 
+    activePlayer===0 ? (activePlayer=1):(activePlayer = 0);
+    
+    //Ulaan tsegiig shiljvvleh 
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    // shoog tvr alga bolgoh 
+    diceDom.style.display="none";
+}
 });
+
+
